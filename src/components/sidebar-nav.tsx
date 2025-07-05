c5ff5e5bf0337b01f6fa7476c7eab8f262cfccd9
Rachmat S. Haryadi <rachmatsharyadi@gmail.com>
2025-07-05 13:01:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarNav>
    
    with

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -32,9 +32,9 @@ export function SidebarNav() {
   return (
     <>
       <SidebarHeader>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 group-data-[collapsible=icon]:justify-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-8 w-8 text-primary"><path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"></path><circle cx="12" cy="13" r="3"></circle></svg>
-          <div className="group-data-[collapsed=icon]:hidden">
+          <div className="group-data-[collapsible=icon]:hidden">
             <h2 className="font-headline text-2xl font-semibold">MeetingSync</h2>
           </div>
         </div>
